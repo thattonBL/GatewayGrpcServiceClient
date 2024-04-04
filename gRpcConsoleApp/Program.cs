6e935ad0ba6d85633d379d571d5188c37669fcb0
thatton <thomasrichardhatton@googlemail.com>
2024-04-04 12:46:34 +0100

    First Commit of demo Grpc .NET Client

---
+++ gRpcConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GatewayGrpcService.Protos;
+
+namespace gRpcConsoleApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Grpc Client here.....!!!");
+            var channel = GrpcChannel.ForAddress("http://localhost:62620");
+            int Count = 0;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            try
+            {
+                var client = new GatewayGrpcService.Protos.GatewayGrpcService.GatewayGrpcServiceClient(channel);
+
+                using var call = client.GetGatewayMessages(new RequestParams(), deadline: DateTime.UtcNow.AddMinutes(10));
+                await foreach (var each in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(String.Format("New Order Receieved from {0}-{1},Order ID = {2}, Unit Price ={3}, Ship Date={4}", each.Author, each.Title, each.StorageLocationCode, each.Shelfmark, each.ReaderName));
+                    Count++;
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.DeadlineExceeded)
+            {
+                Console.WriteLine("Service timeout.");
+            }
+            watch.Stop();
+
+            Console.WriteLine($"Stream ended: Total Records:{Count.ToString()} in {watch.Elapsed.TotalMinutes} minutes and {watch.Elapsed.TotalSeconds} seconds.");
+            Console.Read();
+        }
+    }
+}
